b16c3f2630a56a560052e8826ad5623874d015b5
Vania Atanasova <vaniaatanasova83@gmail.com>
2021-03-15 00:57:32 +0200

    scroll effect

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-
 $('.carousel').carousel({
     interval: 3000
 });
 
+
+const scrollToTopButton = $('.scrollToTop');
+if ($(window).scrollTop() < headerHeight) {
+    scrollToTopButton
+        .css({
+            opacity: 0,
+            pointerEvents: 'none'
+        })
+}
+
+$(window).on('scroll', function () {
+    let scrollTop = $(this).scrollTop();
+
+    if (scrollTop < headerHeight) {
+        scrollToTopButton
+            .css({
+                opacity: 0,
+                pointerEvents: 'none'
+            })
+    } else {
+        scrollToTopButton
+            .css({
+                opacity: 1,
+                pointerEvents: 'initial'
+            })
+    }
+
+
+});
